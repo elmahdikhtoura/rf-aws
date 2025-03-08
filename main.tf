d4abc6f1f66a2567f5b9809ad965ce4490c3b259
elmahdikhtoura <elmahdikhtoura@gmail.com>
2025-03-08 17:42:05 +0100

    6st commit with Terraform configuration

--- main.tf
+++ main.tf
@@ -1,28 +1,26 @@
-# Configurer le fournisseur AWS
 provider "aws" {
   region = "eu-west-3"
 }
 
-# Récupérer l'AMI le plus récent pour Amazon Linux 2
-data "aws_ssm_parameter" "latest_amazon_linux_2" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+# Récupérer l'AMI le plus récent pour Windows Server
+data "aws_ami" "windows-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2019-English-Full-Base-*"]
+  }
 }
 
-# Créer une instance EC2
-resource "aws_instance" "example" {
-  ami           = data.aws_ssm_parameter.latest_amazon_linux_2.value
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.example.id]
-}
-
-# Créer un groupe de sécurité pour l'instance EC2
-resource "aws_security_group" "example" {
-  name        = "example-sg"
-  description = "Allow inbound traffic on port 22"
+# Créer un groupe de sécurité pour l'instance
+resource "aws_security_group" "windows-sg" {
+  name        = "windows-sg"
+  description = "Allow inbound traffic on port 3389"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 3389
+    to_port     = 3389
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -35,22 +33,10 @@ resource "aws_security_group" "example" {
   }
 }
 
-# Créer un VPC (si nécessaire)
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Créer un sous-réseau (si nécessaire)
-resource "aws_subnet" "example" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id     = aws_vpc.example.id
-  availability_zone = "eu-west-3a"
-}
-
-# Associer l'instance EC2 au sous-réseau
-resource "aws_instance" "example_with_subnet" {
-  ami           = data.aws_ssm_parameter.latest_amazon_linux_2.value
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.example.id]
-  subnet_id = aws_subnet.example.id
+# Créer une instance Windows Server
+resource "aws_instance" "windows-server" {
+  ami           = data.aws_ami.windows-ami.id
+  instance_type = "t2.medium"
+  vpc_security_group_ids = [aws_security_group.windows-sg.id]
+  key_name               = "votre_clef_ssh" # Remplacez par votre clé SSH
 }
